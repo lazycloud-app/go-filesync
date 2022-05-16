102b6b42881e8f6ba0fcfdbe4799c962d23bd928
Sergey Krekhno <sergey.krekhno@gmail.com>
2022-05-16 18:22:51 +0300

    Clean config file before and after reading to avoid path errors

--- config/decode.go
+++ config/decode.go
@@ -12,11 +12,20 @@ func Decode_v1(f string) (*ClientConfV1, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	//Replacing possible bad bytes to avoid json errors
+	//Not all users can understand errors like "invalid character 'C' in string escape code"
+	//So it's just good for usability
+	for n, v := range b {
+		if v == '\\' {
+			b[n] = '/'
+		}
+	}
 	err = json.Unmarshal(b, &c)
 	if err != nil {
 		return nil, err
 	}
+	//Avoiding possible bad user input in config file / CLI or cases when user saved config from other filesystem
+	c.EscapeBadFilepaths()
 
 	if c.CONFIG_VER != 1 {
 		return nil, fmt.Errorf("config version mismatch (want 1, got %d", c.CONFIG_VER)
--- config/models.go
+++ config/models.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"path/filepath"
+	"strings"
+)
+
 type (
 	ClientConfV1 struct {
 		CONFIG_VER            int
@@ -14,3 +19,36 @@ type (
 		DB_FILE_NAME          string
 	}
 )
+
+//EscapeBadFilepaths replaces possibly wrong bits in filepaths in case config file has unpredictable separators.
+//Useful to keep single-slashed Windows filepaths in json-encoded config
+func (c *ClientConfV1) EscapeBadFilepaths() {
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, "/", string(filepath.Separator))
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, `\`, string(filepath.Separator))
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.SERVER_CERT_FILE = strings.ReplaceAll(c.SERVER_CERT_FILE, "/", string(filepath.Separator))
+	c.SERVER_CERT_FILE = strings.ReplaceAll(c.SERVER_CERT_FILE, `\`, string(filepath.Separator))
+	c.SERVER_CERT_FILE = strings.ReplaceAll(c.SERVER_CERT_FILE, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.SERVER_ADDRESS = strings.ReplaceAll(c.SERVER_ADDRESS, "/", string(filepath.Separator))
+	c.SERVER_ADDRESS = strings.ReplaceAll(c.SERVER_ADDRESS, `\`, string(filepath.Separator))
+	c.SERVER_ADDRESS = strings.ReplaceAll(c.SERVER_ADDRESS, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.DIR_LOGS = strings.ReplaceAll(c.DIR_LOGS, "/", string(filepath.Separator))
+	c.DIR_LOGS = strings.ReplaceAll(c.DIR_LOGS, `\`, string(filepath.Separator))
+	c.DIR_LOGS = strings.ReplaceAll(c.DIR_LOGS, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.DIR_CACHE = strings.ReplaceAll(c.DIR_CACHE, "/", string(filepath.Separator))
+	c.DIR_CACHE = strings.ReplaceAll(c.DIR_CACHE, `\`, string(filepath.Separator))
+	c.DIR_CACHE = strings.ReplaceAll(c.DIR_CACHE, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, "/", string(filepath.Separator))
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, `\`, string(filepath.Separator))
+	c.FILE_SYSTEM_ROOT_PATH = strings.ReplaceAll(c.FILE_SYSTEM_ROOT_PATH, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+	c.DB_FILE_NAME = strings.ReplaceAll(c.DB_FILE_NAME, "/", string(filepath.Separator))
+	c.DB_FILE_NAME = strings.ReplaceAll(c.DB_FILE_NAME, `\`, string(filepath.Separator))
+	c.DB_FILE_NAME = strings.ReplaceAll(c.DB_FILE_NAME, string(filepath.Separator)+string(filepath.Separator), string(filepath.Separator))
+
+}
